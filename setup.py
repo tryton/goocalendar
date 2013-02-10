f57239b95215db949edf9de98cb3a78bc334d090
Cédric Krier <ced@b2ck.com>
2013-02-10 17:00:42 +0100

    Add coding to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #This file is part of GooCalendar.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
