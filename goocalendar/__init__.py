4b830dfa2e32ee18bef839901afcf2717eec3c42
Cédric Krier <ced@b2ck.com>
2015-02-11 21:24:56 +0100

    Make comment starts with '# '

--- goocalendar/__init__.py
+++ goocalendar/__init__.py
@@ -1,5 +1,5 @@
-#This file is part of GooCalendar.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of GooCalendar.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from ._calendar import Calendar
 from ._event import Event, EventStore
 
