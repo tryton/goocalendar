ddc756e6ccf0ad678baff2ca48fb1b1a8e2d5442
Cédric Krier <ced@b2ck.com>
2023-03-18 13:56:53 +0100

    Prepare for release 0.8.0

--- goocalendar/__init__.py
+++ goocalendar/__init__.py
@@ -12,4 +12,4 @@ from ._calendar import Calendar  # noqa: E402
 from ._event import Event, EventStore  # noqa: E402
 
 __all__ = ['Calendar', 'EventStore', 'Event']
-__version__ = '0.7.3'
+__version__ = '0.8.0'
